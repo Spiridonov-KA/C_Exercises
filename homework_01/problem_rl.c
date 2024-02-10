3795c2f4b82da0c0dfb3a3f5b29153ff8c1b0a84
Kirill Spriridonov <vo-ro@list.ru>
2024-02-11 01:06:42 +0300

    fixed problem_rl for negative numbers

--- homework_01/problem_rl.c
+++ homework_01/problem_rl.c
@@ -2,31 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int iabs(int x) { return (x < 0) ? -x : x; }
+long long llabs(long long x) {
+	return (x < 0) ? -x : x;
+}
 
-int eu_mod(int x, int y) {
-  int r;
-  assert(y != 0);
-  r = x % y;
-  if (r < 0)
-    r += iabs(y);
-  return r;
+long long eu_mod(long long x, long long y) {
+	long long r;
+	assert(y != 0);
+	r = x % y;
+	if (r < 0)
+		r += llabs(y);
+	return r;
 }
 
 #if 0
 long long gcd(long long x, long long y) {
-  long long q;
-  q = eu_mod(x, y);
-  if (q == 0)
-    return y;
-  return gcd(y, q);
+	long long q;
+	q = eu_mod(x, y);
+	if (q == 0)
+	  return y;
+	return gcd(y, q);
 }
 #endif
 
 long long gcd(long long x, long long y) {
 	long long r, tmp;
+	x = llabs(x);
+	y = llabs(y);
 	while (y) {
-		r = eu_mod(x, y);
+		r = x % y;
 		x = y;
 		y = r;
 	}
@@ -34,13 +38,22 @@ long long gcd(long long x, long long y) {
 	return x;
 }
 
+void read_input(long long *x, long long *y) {
+	int res;
+	res = scanf("%lld %lld", x, y);
+	assert(res == 2);
+	if (res != 2) {
+		printf("Incorrect input!\n");
+		abort();
+	}
+}
+
 int main() {
-  long long x = 0, y = 0, g;
-  long long res;
-  
-  res = scanf("%lld %lld", &x, &y);
-  assert(res == 2);
-  g = gcd(x, y);
-  printf("%lld\n", g);
-  return 0;
+	long long x = 0, y = 0, g;
+
+	read_input(&x, &y);
+	g = gcd(x, y);
+
+	printf("%lld\n", g);
+	return 0;
 }
