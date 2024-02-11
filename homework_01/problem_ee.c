27dd731b02bd94f095927cdfdd89b41b2faaab43
Kirill Spriridonov <vo-ro@list.ru>
2024-02-11 13:08:36 +0300

    new solve of task

--- homework_01/problem_ee.c
+++ homework_01/problem_ee.c
@@ -14,6 +14,7 @@ void read_input(long long *x, long long *y) {
 	}
 }
 
+#if 0
 long long extended_gcd(long long a, long long b, long long *x, long long *y) {
 	long long x1, y1, g;
 	if (b == 0) {
@@ -33,6 +34,28 @@ long long extended_gcd(long long a, long long b, long long *x, long long *y) {
 		*y = x1 - y1 * (a / b);
 	}
 
+	return g;
+}
+#endif
+
+long long extended_gcd_main(long long a, long long b, long long *x, long long *y) {
+	long long x1, y1, g;
+	if (!b) {
+		*x = 1;
+		*y = 0;
+		return a;
+	}
+	g = extended_gcd_main(b, a % b, &x1, &y1);
+	*x = y1;
+	*y = x1 - (a / b) * y1;
+	return g;
+}
+
+long long extended_gcd(long long a, long long b, long long *x, long long *y) {
+	long long g;
+	g = extended_gcd_main(llabs(a), llabs(b), x, y);
+	*x *= (a / llabs(a));
+	*y *= (b / llabs(b));
 	return g;
 }
 
